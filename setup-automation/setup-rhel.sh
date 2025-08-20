2f8b7b4f21cfd0d15b12a35b636d7334c503c350
Wilson Harris <wharris@redhat.com>
2025-08-20 11:51:56 -0400

    Packages to install to instances, install tmux for setup script

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-dnf install -y tree
 TMUX='' tmux new-session -d -s 'Terminal1' > /dev/null 2>&1
 tmux set -g pane-border-status top
 tmux setw -g pane-border-format ' #{pane_index} #{pane_current_command}'
